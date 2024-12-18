328e6fa7cd2805260b12a2e97ad9426ebcdafb28
Wyell Garay <wyellgaray11@gmail.com>
2024-12-18 08:00:33 +0800

    Rich Text Editor to Post

--- spartanspaceweb/spartanspacepage/models.py
+++ spartanspaceweb/spartanspacepage/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     title = models.CharField(max_length=225)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     overview = models.TextField(max_length=1000)
     event_date = models.DateTimeField()
-    body = models.TextField()
+    # body = models.TextField()
+    body = RichTextField(blank=True, null=True)
     header_image = models.ImageField(null=True, blank=True, upload_to="images/")
     
 
--- spartanspaceweb/spartanspaceweb/settings.py
+++ spartanspaceweb/spartanspaceweb/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'spartanspacepage',
     'members',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
